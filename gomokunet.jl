44f49f1df2e6d175a6b74faa83a4ea9a70ea2248
yushoteke <pc318@tremont318.localdomain>
2020-02-23 17:57:00 +0000

    residual network

--- gomokunet.jl
+++ gomokunet.jl
@@ -1,43 +1,6 @@
 using Statistics
 using Flux
 
-M=randn(5,5)
-
-function toyNN(x::Array{Bool,3})
-    return (1 .+rand(5,5))/2,tanh(randn())
-end
-
-w,h,c,b=5,5,2,1
-body1=Chain(Conv((3,3),2=>32,pad=(1,1),leakyrelu),
-            Conv((3,3),32=>64,pad=(1,1),leakyrelu),
-            Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-            Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-            x->reshape(x,:,b),
-            Dense(64*w*h,512,leakyrelu))
-head11=Dense(512,w*h,sigmoid)
-head12=Dense(512,1,tanh)
-
-function NN1(x::Array{Float32,4})
-    w,h,c,b=size(x)
-    temp=body1(x)
-    return reshape(head11(temp),w,h,1,b),reshape(head12(temp),1,1,1,b)
-end
-
-body2=Chain(Conv((3,3),2=>32,pad=(1,1),leakyrelu),
-            Conv((3,3),32=>64,pad=(1,1),leakyrelu),
-            Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-            Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-            x->reshape(x,:,size(x,4)),
-            Dense(64*w*h,512,leakyrelu))
-head21=Dense(512,w*h,sigmoid)
-head22=Dense(512,1,tanh)
-
-function NN2(x::Array{Float32,4})
-    w,h,c,b=size(x)
-    temp=body2(x)
-    return reshape(head21(temp),w,h,1,b),reshape(head22(temp),1,1,1,b)
-end
-
 struct player
     body
     head1
@@ -46,16 +9,50 @@ end
 
 Flux.@treelike player
 
-player(n)=player(Chain(Conv((3,3),2=>32,pad=(1,1),leakyrelu),
-                Conv((3,3),32=>64,pad=(1,1),leakyrelu),
-                Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-                Conv((3,3),64=>64,pad=(1,1),leakyrelu),
-                x->reshape(x,:,size(x,4)),
-                Dense(64*n*n,512,leakyrelu)),Dense(512,n*n,sigmoid),Dense(512,1,tanh))
-
+function player(n;type="conv")
+    if type=="conv"
+        return player(Chain(Conv((3,3),2=>32,pad=(1,1),leakyrelu),
+                        Conv((3,3),32=>64,pad=(1,1),leakyrelu),
+                        Conv((3,3),64=>64,pad=(1,1),leakyrelu),
+                        Conv((3,3),64=>64,pad=(1,1),leakyrelu),
+                        x->reshape(x,:,size(x,4)),
+                        Dense(64*n*n,512,leakyrelu)),Dense(512,n*n,sigmoid),Dense(512,1,tanh))
+    elseif type=="res"
+        return player(Chain(Conv((3,3),2=>128,pad=(1,1),gelu),
+                            ResidualBlock(128),
+                            ResidualBlock(128),
+                            ResidualBlock(128),
+                            ResidualBlock(128),
+                            ResidualBlock(128),
+                            Conv((1,1),128=>4),
+                            BatchNorm(4),
+                            x->gelu.(x),
+                            x->reshape(x,:,size(x,4))),
+                            Dense(4*n*n,n*n,sigmoid),
+                            Chain(Dense(4*n*n,n*n,gelu),Dense(n*n,1,tanh)))
+    end
+end
 
 function (p::player)(x)
     w,h,c,b=size(x)
     tmp=p.body(x)
     return reshape(p.head1(tmp),w,h,1,b),reshape(p.head2(tmp),1,1,1,b)
+end
+
+struct ResidualBlock
+    conv1
+    norm1
+    conv2
+    norm2
+end
+
+Flux.@treelike ResidualBlock
+
+ResidualBlock(in)=ResidualBlock(Conv((3,3),in=>in,pad=(1,1)),
+                                BatchNorm(in),
+                                Conv((3,3),in=>in,pad=(1,1)),
+                                BatchNorm(in))
+
+function (c::ResidualBlock)(x)
+    return gelu.(x.+(gelu.(x|>c.conv1|>c.norm1)|>c.conv2|>c.norm2))
 end
